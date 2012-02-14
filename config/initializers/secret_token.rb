ea8876919fb586d615f6788d282c1c3794da17ee
dsiltman <toymom@toymom.com>
2012-02-13 19:13:13 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TallShip::Application.config.secret_token = 'ce0e0340a5ea35731ac2f721d799dbeb3881df87d04cf6b60540909c75c070d3f641b31742bcd95b7463face7d00cf6bb601d266e226054f6bc47a8f5e69b3f6'
